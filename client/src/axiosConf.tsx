acee74e9c5646898801ecef2168143933c0b5b45
Ivan Antoff <ivan.antoff@gmail.com>
2021-10-25 12:15:35 -0300

    .

--- client/src/axiosConf.tsx
+++ client/src/axiosConf.tsx
@@ -1,9 +1,7 @@
 const axios = require('axios');
 export const axiosInstance = axios.create({
-  credentials: 'include',
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
-    'Accept': 'application/json'
   },
 });
