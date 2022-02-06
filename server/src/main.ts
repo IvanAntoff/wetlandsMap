8370da9238a34051a6be31bece56fde793c4f616
Ivan Antoff <ivan.antoff@gmail.com>
2022-02-06 19:11:14 -0300

    cors object

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,21 +6,19 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 export const FILE_FOLDER = process.env.FILE_FOLDER || join(__dirname,'..','uploads');
 export const URL_SV = process.env.FILES_URL || 'http://localhost:3001/uploads/';
 
+
+console.log(`FILE_FOLDER: ${FILE_FOLDER}`);
+console.log(`URL_SV: ${URL_SV}`);
+console.log(`MONGO_URL: ${process.env.MONGO_URL || 'mongodb://localhost/humedales_posts'}`);
+
+
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: {
-      "origin": ["https://humedalesdigitalescuencadelgualeguaychu.com/", /\.humedalesdigitalescuencadelgualeguaychu\.com$/, "68.233.231.176"],
-      "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-      "preflightContinue": false,
-    } 
-  });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(FILE_FOLDER,{prefix: '/uploads'});
-  app.enableCors();
-
-  console.log(`FILE_FOLDER: ${FILE_FOLDER}`);
-  console.log(`URL_SV: ${URL_SV}`);
-  console.log(`MONGO_URL: ${process.env.MONGO_URL || 'mongodb://localhost/humedales_posts'}`);
-
+  app.enableCors({
+    origin: ["https://humedalesdigitalescuencadelgualeguaychu.com/", /\.humedalesdigitalescuencadelgualeguaychu\.com$/, "http://humedalesdigitalescuencadelgualeguaychu.com/","68.233.231.176"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  });
   await app.listen(3001);
 }
 
